4c2792785f2b05f55ba4fa0c2c5d7f39c0a584e6
sanjeewat847 <116448181+sanjeewat847@users.noreply.github.com>
2024-08-16 17:14:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,21 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/d7M2QQN/IMG-20240809-WA0039-1.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || 
+"```🎩Hello I am Alive Now !```
+
+"*_•°•🍒Have a nice day.🍒•°•_*
+
+"`🤖Bot Name` : *_Dark-Hat-Md_*
+
+"`⚙️Version` : *_1.0.0_*
+
+"`👨🏻‍💻Owner` : *_Mr Dinuwa_*
+ 
+"`👾Github` : *_github.com/dinuwaofficial2007_* 
+
+"*_🙂Thanks for using Me.🙂_*
+
+"*© 🎩Dark-Hat-Md 2024🎩*",  
 };
